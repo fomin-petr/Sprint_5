bdc8577ac9e379cfb94220371da95703d67657dd
petr-fomin <petr.fomin0405@gmail.com>
2023-11-01 00:38:42 +0100

    Sprint_5 tests

---
+++ tests/conftest.py
@@ -0,0 +1,53 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+from tests.locators import StartPageLocators, LoginPageLocators, RegistrationPageLocators
+import random
+from random import choice
+from string import ascii_letters
+
+
+@pytest.fixture(scope='session')
+def registration(random_email, random_password):
+    email = random_email
+    password = random_password
+    driver = webdriver.Chrome()
+    driver.get('https://stellarburgers.nomoreparties.site/')
+    driver.find_element(By.XPATH, StartPageLocators.LOGIN_BUTTON).click()
+    WebDriverWait(driver, 3).until(
+        expected_conditions.visibility_of_element_located((By.XPATH, LoginPageLocators.REGISTRATION_BUTTON)))
+    driver.find_element(By.XPATH, LoginPageLocators.REGISTRATION_BUTTON).click()
+    WebDriverWait(driver, 3).until(
+        expected_conditions.visibility_of_element_located((By.XPATH, RegistrationPageLocators.NAME_INPUT_FIELD)))
+    driver.find_element(By.XPATH, RegistrationPageLocators.NAME_INPUT_FIELD).send_keys('Test')
+    driver.find_element(By.XPATH, RegistrationPageLocators.EMAIL_INPUT_FIELD).send_keys(email)
+    driver.find_element(By.XPATH, RegistrationPageLocators.PASSWORD_INPUT_FIELD).send_keys(password)
+    driver.find_element(By.XPATH, RegistrationPageLocators.REGISTRATION_BUTTON).click()
+    yield driver
+    driver.quit()
+
+
+@pytest.fixture()
+def start_page():
+    driver = webdriver.Chrome()
+    driver.get('https://stellarburgers.nomoreparties.site/')
+    driver.maximize_window()
+    yield driver
+    driver.quit()
+
+
+@pytest.fixture(scope='session')
+def random_email():
+    random_email = ''
+    for i in range(8):
+        random_email += choice(ascii_letters)
+    random_email += '__' + str(random.randint(0, 99999999)) + '@yaya.ru'
+    return random_email
+
+
+@pytest.fixture(scope='session')
+def random_password():
+    random_password = random.randint(100000, 999999)
+    return random_password
---
+++ tests/test_loggin_in.py
@@ -0,0 +1,72 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+from locators import StartPageLocators, LoginPageLocators, RegistrationPageLocators, ForgotPasswordPageLocators
+
+
+def test_login_in_main_page(registration, start_page, random_email, random_password):
+    driver = start_page
+    driver.find_element(By.XPATH, StartPageLocators.LOGIN_BUTTON).click()
+    WebDriverWait(driver, 3).until(
+        expected_conditions.visibility_of_element_located((By.XPATH, LoginPageLocators.LOGIN_BUTTON)))
+    driver.find_element(By.XPATH, LoginPageLocators.EMAIL_INPUT_FIELD).send_keys(random_email)
+    driver.find_element(By.XPATH, LoginPageLocators.PASSWORD_INPUT_FIELD).send_keys(random_password)
+    driver.find_element(By.XPATH, LoginPageLocators.LOGIN_BUTTON).click()
+    WebDriverWait(driver, 3).until(
+        expected_conditions.visibility_of_element_located((By.XPATH, StartPageLocators.CREATE_ORDER_BUTTON)))
+    assert driver.current_url == 'https://stellarburgers.nomoreparties.site/'
+    driver.quit()
+
+
+def test_login_by_click_private_page(start_page, random_email, random_password):
+    driver = start_page
+    driver.find_element(By.XPATH, StartPageLocators.PRIVATE_PAGE_BUTTON).click()
+    WebDriverWait(driver, 3).until(
+        expected_conditions.visibility_of_element_located((By.XPATH, LoginPageLocators.LOGIN_BUTTON)))
+    driver.find_element(By.XPATH, LoginPageLocators.EMAIL_INPUT_FIELD).send_keys(random_email)
+    driver.find_element(By.XPATH, LoginPageLocators.PASSWORD_INPUT_FIELD).send_keys(random_password)
+    driver.find_element(By.XPATH, LoginPageLocators.LOGIN_BUTTON).click()
+    WebDriverWait(driver, 3).until(
+        expected_conditions.visibility_of_element_located((By.XPATH, StartPageLocators.CREATE_ORDER_BUTTON)))
+    assert driver.current_url == 'https://stellarburgers.nomoreparties.site/'
+    driver.quit()
+
+
+def test_login_from_registration_page(start_page, random_email, random_password):
+    driver = start_page
+    driver.find_element(By.XPATH, StartPageLocators.LOGIN_BUTTON).click()
+    WebDriverWait(driver, 3).until(
+        expected_conditions.visibility_of_element_located((By.XPATH, LoginPageLocators.REGISTRATION_BUTTON)))
+    driver.find_element(By.XPATH, LoginPageLocators.REGISTRATION_BUTTON).click()
+    WebDriverWait(driver, 3).until(
+        expected_conditions.visibility_of_element_located((By.XPATH, RegistrationPageLocators.NAME_INPUT_FIELD)))
+    driver.find_element(By.XPATH, RegistrationPageLocators.LOGIN_BUTTON).click()
+    WebDriverWait(driver, 3).until(
+        expected_conditions.visibility_of_element_located((By.XPATH, LoginPageLocators.LOGIN_BUTTON)))
+    driver.find_element(By.XPATH, LoginPageLocators.EMAIL_INPUT_FIELD).send_keys(random_email)
+    driver.find_element(By.XPATH, LoginPageLocators.PASSWORD_INPUT_FIELD).send_keys(random_password)
+    driver.find_element(By.XPATH, LoginPageLocators.LOGIN_BUTTON).click()
+    WebDriverWait(driver, 3).until(
+        expected_conditions.visibility_of_element_located((By.XPATH, StartPageLocators.CREATE_ORDER_BUTTON)))
+    assert driver.current_url == 'https://stellarburgers.nomoreparties.site/'
+    driver.quit()
+
+
+def test_login_from_password_recovery(start_page, random_email, random_password):
+    driver = start_page
+    driver.find_element(By.XPATH, StartPageLocators.LOGIN_BUTTON).click()
+    WebDriverWait(driver, 3).until(
+        expected_conditions.visibility_of_element_located((By.XPATH, LoginPageLocators.REGISTRATION_BUTTON)))
+    driver.find_element(By.XPATH, LoginPageLocators.FORGOT_PASSWORD_BUTTON).click()
+    WebDriverWait(driver, 3).until(
+        expected_conditions.visibility_of_element_located((By.XPATH, ForgotPasswordPageLocators.LOGIN_BUTTON)))
+    driver.find_element(By.XPATH, ForgotPasswordPageLocators.LOGIN_BUTTON).click()
+    WebDriverWait(driver, 3).until(
+        expected_conditions.visibility_of_element_located((By.XPATH, LoginPageLocators.LOGIN_BUTTON)))
+    driver.find_element(By.XPATH, LoginPageLocators.EMAIL_INPUT_FIELD).send_keys(random_email)
+    driver.find_element(By.XPATH, LoginPageLocators.PASSWORD_INPUT_FIELD).send_keys(random_password)
+    driver.find_element(By.XPATH, LoginPageLocators.LOGIN_BUTTON).click()
+    WebDriverWait(driver, 3).until(
+        expected_conditions.visibility_of_element_located((By.XPATH, StartPageLocators.CREATE_ORDER_BUTTON)))
+    assert driver.current_url == 'https://stellarburgers.nomoreparties.site/'
+    driver.quit()
