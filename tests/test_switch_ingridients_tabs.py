c713244721a65f90de9092f1df13a679a3549f2c
fomin-petr <petr.fomin0405@gmail.com>
2023-11-02 22:12:52 +0100

    Test_switch_ingridients_tabs update

--- tests/test_switch_ingridients_tabs.py
+++ tests/test_switch_ingridients_tabs.py
@@ -5,19 +5,16 @@ from locators import StartPageLocators
 class TestSwitchConstructorTabs:
     def test_switch_to_sauce(self, start_page):
         driver = start_page
-        sauce_button_class = driver.find_element(By.XPATH, StartPageLocators.SAUCE_TAB).get_attribute('class')
         driver.find_element(By.XPATH, StartPageLocators.SAUCE_TAB).click()
-        assert driver.find_element(By.XPATH, StartPageLocators.SAUCE_TAB).get_attribute('class') != sauce_button_class
+        assert 'tab_tab_type_current' in driver.find_element(By.XPATH, StartPageLocators.SAUCE_TAB).get_attribute('class')
 
     def test_switch_to_ingredients(self, start_page):
         driver = start_page
-        ingredients_button_class = driver.find_element(By.XPATH, StartPageLocators.INGREDIENTS_TAB).get_attribute('class')
         driver.find_element(By.XPATH, StartPageLocators.INGREDIENTS_TAB).click()
-        assert driver.find_element(By.XPATH, StartPageLocators.INGREDIENTS_TAB).get_attribute('class') != ingredients_button_class
+        assert 'tab_tab_type_current' in driver.find_element(By.XPATH, StartPageLocators.INGREDIENTS_TAB).get_attribute('class')
 
     def test_switch_to_buns(self, start_page):
         driver = start_page
-        buns_button_class = driver.find_element(By.XPATH, StartPageLocators.BUN_TAB).get_attribute('class')
         driver.find_element(By.XPATH, StartPageLocators.SAUCE_TAB).click()
         driver.find_element(By.XPATH, StartPageLocators.BUN_TAB).click()
-        assert driver.find_element(By.XPATH, StartPageLocators.BUN_TAB).get_attribute('class') == buns_button_class
+        assert 'tab_tab_type_current' in driver.find_element(By.XPATH, StartPageLocators.BUN_TAB).get_attribute('class')
